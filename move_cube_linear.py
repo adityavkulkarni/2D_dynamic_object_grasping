599eed3b10de7041aeb9a14d4cfc81b3452f58f2
adityavkulkarni <adityavkulkarni0999@gmail.com>
2024-11-01 19:22:16 -0500

    circular

--- move_cube_linear.py
+++ move_cube_linear.py
@@ -4,6 +4,7 @@
 CS 6301 Homework 4 Programming
 Robot Control for Grasping
 """
+import math
 from grasp import get_pose_gazebo, set_model_pose
 import time
 import time
@@ -14,10 +15,10 @@ from gazebo_msgs.msg import ModelState
 
 
 class CubeMover:
-    def __init__(self, model_name='demo_cube'):
+    def __init__(self, model_name='demo_cube', motion_type='linear', velocity=0.1):
         self.model_name = model_name
         self.box_pose = None
-        self.incr = -0.001
+        self.incr = -velocity * 0.01
         self.iter = 10
         self.running = False
 
@@ -42,11 +43,8 @@ class CubeMover:
         self.running = True
         self.box_pose = self.get_pose()
         rospy.loginfo("Cube movement starting")
-        """self.box_pose.position.x = 0.6
-        self.box_pose.position.y = 0.4
-        self.box_pose.position.z = 0.724341"""
 
-        def move():
+        def move_linear():
             while self.running:
                 if self.box_pose.position.y <= -0.4 or self.box_pose.position.y > 0.4:
                     self.iter -= 1
@@ -58,8 +56,29 @@ class CubeMover:
                     break
                 time.sleep(0.01)
 
+        def move_circular():
+            angle = 0  # Initial angle in radians
+            radius = 0.4  # Radius of the circular path
+            angular_speed = self.incr  # Speed at which the angle increases
+
+            while self.running:
+                # Update the x and y positions based on the current angle
+                self.box_pose.position.x = radius * math.cos(angle)
+                self.box_pose.position.y = radius * math.sin(angle)
+
+                # Set the new pose
+                ret = self.set_pose()
+                if ret == -1:
+                    break
+
+                # Increment the angle for the next iteration
+                angle += angular_speed
+
+                # Sleep for a short duration to control the speed of movement
+                time.sleep(0.01)
+
         # Start the movement in a new thread
-        thread = threading.Thread(target=move)
+        thread = threading.Thread(target=move_linear)
         thread.start()
         self.thread = thread
 
